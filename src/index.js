51580325c62acc013001fa66e2e54cea59be24fe
gomaci75 <gomaci75@gmail.com>
2025-04-21 20:39:33 +0900

    first commit1

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <App />
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
