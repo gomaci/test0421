157350fe2e9be8df17cfa408ea1519e5a1abd636
gomaci75 <gomaci75@gmail.com>
2025-04-21 20:47:57 +0900

    다시업로드2

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+<React.StrictMode>
   <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+</React.StrictMode>
 );
 
